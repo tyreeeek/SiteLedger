08f2364bdef9ad5c1175f96a36fdcf5084e8b6d0
Tariq Jawaz Khan <tariqjk41@gmail.com>
2025-11-18 16:57:14 -0500

    Initial Commit

---
+++ Crewnex/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Crewnex
+//
+//  Created by Zia  on 11/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
